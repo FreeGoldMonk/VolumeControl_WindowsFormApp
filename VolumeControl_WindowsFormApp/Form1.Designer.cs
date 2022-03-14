5b6155eb6730a1c8c5614998295e6f8e91285727
Christian <cibeijiuduzhongsheng@gmail.com>
2022-03-14 10:40:54 +0100

    Add project files.

---
+++ VolumeControl_WindowsFormApp/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿/*
+//2022-03-13: https://www.dotnetcurry.com/ShowArticle.aspx?ID=431
+//Programmatically Increase, Decrease and Mute the Volume
+//Försökte, men denna appen fick jag inte att fungera
+using System;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace VolumeControl_WindowsFormApp
+{
+    public partial class Form1 : Form
+    {
+        private const int APPCOMMAND_VOLUME_MUTE = 0x80000;
+        private const int APPCOMMAND_VOLUME_UP = 0xA0000;
+        private const int APPCOMMAND_VOLUME_DOWN = 0x90000;
+        private const int WM_APPCOMMAND = 0x319;
+
+        [DllImport("user32.dll")] //c:\Windows\System32\user32.dll
+        public static extern IntPtr SendMessageW(IntPtr hWnd, int Msg,
+            IntPtr wParam, IntPtr lParam);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMute_Click(object sender, EventArgs e)
+        {
+            SendMessageW(this.Handle, WM_APPCOMMAND, this.Handle,
+                (IntPtr)APPCOMMAND_VOLUME_MUTE);
+        }
+
+        private void btnDecVol_Click(object sender, EventArgs e)
+        {
+            SendMessageW(this.Handle, WM_APPCOMMAND, this.Handle,
+                (IntPtr)APPCOMMAND_VOLUME_DOWN);
+        }
+
+        private void btnIncVol_Click(object sender, EventArgs e)
+        {
+            SendMessageW(this.Handle, WM_APPCOMMAND, this.Handle,
+                (IntPtr)APPCOMMAND_VOLUME_UP);
+        }
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMute = new System.Windows.Forms.Button();
+            this.btnDecVol = new System.Windows.Forms.Button();
+            this.btnIncVol = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnMute
+            // 
+            this.btnMute.Location = new System.Drawing.Point(41, 24);
+            this.btnMute.Name = "btnMute";
+            this.btnMute.Size = new System.Drawing.Size(187, 82);
+            this.btnMute.TabIndex = 0;
+            this.btnMute.Text = "Mute";
+            this.btnMute.UseVisualStyleBackColor = true;
+            // 
+            // btnDecVol
+            // 
+            this.btnDecVol.Location = new System.Drawing.Point(42, 153);
+            this.btnDecVol.Name = "btnDecVol";
+            this.btnDecVol.Size = new System.Drawing.Size(186, 76);
+            this.btnDecVol.TabIndex = 1;
+            this.btnDecVol.Text = "Decrese volume";
+            this.btnDecVol.UseVisualStyleBackColor = true;
+            // 
+            // btnIncVol
+            // 
+            this.btnIncVol.Location = new System.Drawing.Point(42, 275);
+            this.btnIncVol.Name = "btnIncVol";
+            this.btnIncVol.Size = new System.Drawing.Size(189, 70);
+            this.btnIncVol.TabIndex = 2;
+            this.btnIncVol.Text = "Increase volume";
+            this.btnIncVol.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(423, 439);
+            this.Controls.Add(this.btnIncVol);
+            this.Controls.Add(this.btnDecVol);
+            this.Controls.Add(this.btnMute);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button btnMute;
+        private Button btnDecVol;
+        private Button btnIncVol;
+    }
+}
+*/
---
+++ VolumeControl_WindowsFormApp/Form1.cs
@@ -0,0 +1,74 @@
+﻿//2022-03-13: https://stackoverflow.com/questions/36331431/c-sharp-windows-forms-application-volume-slider
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace VolumeControl_WindowsFormApp
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("winmm.dll")]
+        public static extern int waveOutGetVolume(IntPtr hwo, out uint dwVolume);
+
+        [DllImport("winmm.dll")]
+        public static extern int waveOutSetVolume(IntPtr hwo, uint dwVolume);
+
+        public Form1()
+        {
+            InitializeComponent();
+            // By the default set the volume to 0
+            uint CurrVol = 0;
+            // At this point, CurrVol gets assigned the volume
+            waveOutGetVolume(IntPtr.Zero, out CurrVol);
+            // Calculate the volume
+            ushort CalcVol = (ushort)(CurrVol & 0x0000ffff);
+            // Get the volume on a scale of 1 to 10 (to fit the trackbar)
+            trackBar1.Value = CalcVol / (ushort.MaxValue / 10);
+            /*trackWave.Value = CalcVol / (ushort.MaxValue / 10);*/
+        }
+        /*private void trackWave_Scroll(object sender, EventArgs e)*/
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            // Calculate the volume that's being set. BTW: this is a trackbar!
+            int NewVolume = ((ushort.MaxValue / 10) * trackBar1.Value);
+            /*int NewVolume = ((ushort.MaxValue / 10) * trackWave.Value);*/
+            // Set the same volume for both the left and the right channels
+            uint NewVolumeAllChannels = (((uint)NewVolume & 0x0000ffff) | ((uint)NewVolume << 16));
+            // Set the volume
+            waveOutSetVolume(IntPtr.Zero, NewVolumeAllChannels);
+        }
+
+        private TrackBar trackBar1;
+
+        private void InitializeComponent()
+        {
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(36, 33);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(214, 45);
+            this.trackBar1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.trackBar1);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}
